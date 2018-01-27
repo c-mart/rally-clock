3e0aa2ea6204f58f9d449df2a6382f4cc3ae17dd
chris <chris@c-mart.in>
2018-01-27 07:39:34 -0500

    initial commit

---
+++ RallyClock.elm
@@ -0,0 +1,84 @@
+{-
+   Three things:
+   - GPS odometer (not doing tonight)
+   -
+-}
+
+
+module Main exposing (..)
+
+import Date
+import Html exposing (Html, program)
+import Time
+
+
+main =
+    program { init = init, view = view, update = update, subscriptions = subs }
+
+
+type alias Model =
+    Time.Time
+
+
+view : Model -> Html Msg
+view model =
+    let
+        date =
+            Date.fromTime model
+
+        hours =
+            Date.hour date
+
+        mins =
+            Date.minute date
+
+        secs =
+            Date.second date
+
+        millisecs =
+            Date.millisecond date
+
+        preciseSecs =
+            (toFloat secs) + (toFloat millisecs / 1000)
+
+        hundredthsOfMins =
+            secsToHundredths preciseSecs
+
+        timeText =
+            (intToPadStr hours) ++ " : " ++ (intToPadStr mins) ++ " . " ++ (intToPadStr hundredthsOfMins)
+    in
+        Html.text timeText
+
+
+secsToHundredths : Float -> Int
+secsToHundredths secs =
+    secs
+        / 60
+        * 100
+        |> floor
+
+
+intToPadStr : Int -> String
+intToPadStr int =
+    String.padLeft 2 '0' (toString int)
+
+
+type Msg
+    = Tick Time.Time
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        Tick newTime ->
+            ( newTime, Cmd.none )
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( 0, Cmd.none )
+
+
+subs : Model -> Sub Msg
+subs model =
+    Time.every (Time.minute / 500) Tick
